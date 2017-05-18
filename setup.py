5bd7c9987abcdd68c591f3bd4ea50d899eefe25a
loveyuu <linbin1253007885@gmail.com>
2017-05-19 00:58:40 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 print("program start.....")
+
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+
+if __name__ == "__main__":
+    stu = Student("linbin")
+    print(stu.name)
+
